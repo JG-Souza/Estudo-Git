130422020414103f3573b46785e4f522710ae67f
JG-Souza <joaogsouzafma@gmail.com>
2024-09-27 10:54:25 -0300

    add feature soma

---
+++ git.py
@@ -0,0 +1,10 @@
+# Crie um repositório local e faça uma série de commits com mensagens descritivas. Clone um repositório remoto e faça um pull.
+
+lista_numeros = [1, 2, 3, 4, 5]
+
+def soma(*numeros):
+    resultado = sum(numeros)
+
+    return resultado
+
+print(soma(*lista_numeros))
