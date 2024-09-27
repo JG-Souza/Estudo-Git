c47f3d1d3bfe4534da5c8e73dcbe94e86eac3af4
JG-Souza <joaogsouzafma@gmail.com>
2024-09-27 11:04:47 -0300

    fix soma, add subtrai

--- git.py
+++ git.py
@@ -1,10 +1,15 @@
 # Crie um repositório local e faça uma série de commits com mensagens descritivas. Clone um repositório remoto e faça um pull.
 
-lista_numeros = [1, 2, 3, 4, 5]
-
-def soma(*numeros):
-    resultado = sum(numeros)
+def soma(a, b):
+    resultado = a + b
 
     return resultado
 
-print(soma(*lista_numeros))
+print(soma(2, 3))
+
+def subtrai(a, b):
+    resultado = a- b
+
+    return resultado
+
+print(subtrai(5, 2))
