f3d0eada90745eac313b3271f7a08fd0dc69489b
JG-Souza <joaogsouzafma@gmail.com>
2024-09-28 15:49:49 -0300

    apenas soma aqui

--- git.py
+++ git.py
@@ -1,25 +1,7 @@
-# Crie um repositório local e faça uma série de commits com mensagens descritivas. Clone um repositório remoto e faça um pull.
-
 def soma(a, b):
     resultado = a + b
 
     return resultado
 
 print(soma(2, 3))
-
-def subtrai(a, b):
-    resultado = a- b
-
-    return resultado
-
-print(subtrai(5, 2))
-
-
-def multiplica(a, b):
-    resultado = a * b
-
-    return resultado
-
-print(multiplica(2, 5))
-
 
